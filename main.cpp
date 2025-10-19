debdbe4b7bdd1eef41d37e7df1c4b5dccbbdcb32
Harkaran Gill <harkarangill321@gmail.com>
2025-10-18 20:22:22 -0400

    Minor changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <chrono>
-
 #include "rt.h"
-
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
@@ -9,17 +7,49 @@
 #include "sphere.h"
 
 
-int main() {
-    auto time_start = std::chrono::system_clock::now();
-    //set the world
-    hittable_list world;
+static void scene1(hittable_list& world, camera& cam) {
+    auto ground_material     = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    auto material_dielectric = make_shared<dielectric>(1.5);
+    auto material_bubble     = make_shared<dielectric>(1.0/1.5);
+    auto material_metal      = make_shared<metal>(color(0.2,0.5,0.7), 0);
+    auto material_lambertian = make_shared<lambertian>(color(0.2, 0.2, 0.8));
 
+    auto sphere_ground  = make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material);
+    auto sphere_left    = make_shared<sphere>(point3(-1,0.5,-1), 0.49, material_dielectric);
+    auto sphere_bubble  = make_shared<sphere>(point3(-1,0.5,-1), 0.45, material_bubble);
+    auto sphere_middle  = make_shared<sphere>(point3(0,0.5,-1), 0.49, material_metal);
+    auto sphere_right   = make_shared<sphere>(point3(1,0.5,-1), 0.49, material_lambertian);
+
+    world.add(sphere_ground);
+    world.add(sphere_left);
+    // world.add(sphere_bubble);
+    world.add(sphere_middle);
+    world.add(sphere_right);
+
+
+    cam.aspect_ratio    = 16.0 / 9.0;
+    cam.image_width     = 1000;
+
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 20;
+
+    cam.vfov     = 60;
+    cam.lookfrom = point3(0, 0.5, 1);
+    cam.lookat   = point3(0 ,0.45 ,-1);
+    cam.vup      = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0.0;
+    cam.focus_dist    = 10.0;
+
+}
+
+static void scene2(hittable_list& world, camera& cam) {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
     for (int a = -11; a < 11; ++a) {
         for (int b = -11; b < 11; ++b) {
-            auto choose_mat = random_double();      // choose material based on probability
+            auto choose_mat = random_double(0, 1.0);      // choose material based on probability
 
             // choose position on the plane with some randomness
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
@@ -50,18 +80,16 @@ int main() {
     }
 
     auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0,1,0), 1.0, material1));
-
     auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0);
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
-    camera cam;
-
     cam.aspect_ratio    = 16.0 / 9.0;
-    cam.image_width     = 800;
+    cam.image_width     = 500;
+
     cam.samples_per_pixel = 50;
     cam.max_depth         = 10;
 
@@ -70,13 +98,32 @@ int main() {
     cam.lookat   = point3(0 ,0 ,0);
     cam.vup      = vec3(0, 1, 0);
 
-    cam.defocus_angle = 0;
+    cam.defocus_angle = 0.0;
     cam.focus_dist    = 10.0;
+}
 
+int main() {
+    //set the world
+    hittable_list world;
+    camera cam;
+    while (true) {
+        std::cout << "Please enter the scene number to render: " << std::endl;
+        std::cout << "1: Scene-1, Simple scene with only 3-Spheres" << std::endl;
+        std::cout << "2: Scene-2, A more complex scene with more than 50 spheres" << std::endl;
+        int choice;
+        std::cin >> choice;
+        if (choice == 1) {
+            scene1(world, cam);
+            break;
+        }
+        if (choice == 2) {
+            scene2(world, cam);
+            break;
+        }
+        else {
+            std::cout << "Please enter a valid choice number" << std::endl;
+        }
+    }
     cam.render(world);
-    auto time_end = std::chrono::system_clock::now();
-    auto time = time_end - time_start;
-
-    std::clog << "Time taken to render: " << (float) std::chrono::duration_cast<std::chrono::milliseconds>(time).count()
-            / 1000.0f << std::endl;
+    return 0;
 }
