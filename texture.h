35375d94f09686de20a507a099da4e0b697ca7bd
Harkaran Gill <harkarangill321@gmail.com>
2025-11-04 21:50:02 -0500

    Perlin noise completed

--- texture.h
+++ texture.h
@@ -81,14 +81,15 @@ private:
 
 class noise_texture : public texture {
 public:
-    noise_texture() = default;
+    explicit noise_texture(double scale) : scale(scale) {}
 
     [[nodiscard]] color value(double u, double v, const point3 &p) const override {
-        return color(1, 1, 1) * noise.noise(p);
+        return color(0.5, 0.5, 0.5) * (1 + std::sin(scale * p.z() + 10 * noise.turb(p, 7)));
     }
 
 private:
     perlin noise;
+    double scale;
 };
 
 #endif //TEXTURE_H
